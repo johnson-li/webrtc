024dff6bee91b3229e7f1d9b1ca467ed49a3b5b0
Johnson <johnsonli1993@gmail.com>
2020-04-28 02:22:09 +0300

    set timestamp on frame

--- examples/peerconnection_headless/client/conductor.h
+++ examples/peerconnection_headless/client/conductor.h
@@ -17,12 +17,16 @@
 #include <string>
 #include <vector>
 #include <chrono>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 #include "api/media_stream_interface.h"
 #include "api/peer_connection_interface.h"
 #include "examples/peerconnection_headless/client/peer_connection_client.h"
 #include "base/debug/stack_trace.h"
 #include "rtc_base/logging.h"
+#include "third_party/libyuv/include/libyuv/convert_from.h"
 
 namespace webrtc {
 class VideoCaptureModule;
@@ -32,21 +36,97 @@ namespace cricket {
 class VideoRenderer;
 }  // namespace cricket
 
+#define BUFFER_SIZE 100 * 1024 * 1024
+#define CONTENT_SIZE BUFFER_SIZE - 2048
+struct shared_frames {
+  uint32_t size;
+  uint32_t offset;
+  struct {
+    uint32_t offset;
+    uint32_t length;
+    uint32_t timestamp;
+    uint8_t finished;
+  } indexes[128];
+  uint8_t padding[376];
+  uint8_t content[CONTENT_SIZE];
+};
+
 class VideoRenderer : public rtc::VideoSinkInterface<webrtc::VideoFrame> {
  public:
   VideoRenderer(std::string name, webrtc::VideoTrackInterface* track_to_render)
-      : name_(name), width_(0), height_(0), rendered_track_(track_to_render)  {
+      : name_(name), width_(0), height_(0), shared_frames_(nullptr), rendered_track_(track_to_render)  {
     rendered_track_->AddOrUpdateSink(this, rtc::VideoSinkWants());
+    initSharedMemory();
   }
 
   ~VideoRenderer() {
     rendered_track_->RemoveSink(this);
   }
 
+  /**
+   * Layout of the shared index memory
+   *
+   *  0                   1                   2                   3
+   *  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 
+   * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   * |   S   |   OO  |   O   |   L   |   T   |F|      ...            |
+   * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   * S (4 bytes) = number of the indexes
+   * OO (4 bytes) = offset (in bytes) in the data memory of the next frame
+   * O (4 bytes) = offset (in bytes) in the data memory of the corresponding frame
+   * L (4 bytes) = length (in bytes) in the data memory of the corresponding frame
+   * T (4 bytes) = timestamp (in milliseconds) of the corresponding frame
+   * F (1 bytes) = indicating if writting to the data memory of the corresponding frame is finished 
+   * *Capacity = 128 indexes
+   * *Size = 4 + 4 + (4 + 4 + 4 + 1) * 128 = 1672 bytes
+   * *Wrapped Size = 2048 bytes
+   * *Padding = 2048 - 1672 = 376 bytes 
+   *
+   * Layout of the shared data memory
+   * *Size = 10 * 1024 * 1024 - 2048 = 10483712 bytes 
+   *
+   * *Total Size = 10 MB
+   */
+  void initSharedMemory() {
+    int fd_shm;
+    if ((fd_shm = shm_open("/webrtc_frames", O_RDWR|O_CREAT, 0660)) == -1) {
+      perror("shm_open"); 
+    }
+    if (ftruncate(fd_shm, sizeof(struct shared_frames)) == -1) {
+      perror("ftruncate"); 
+    }
+    if ((shared_frames_ = (struct shared_frames*) mmap(NULL, sizeof(struct shared_frames), PROT_READ | PROT_WRITE, MAP_SHARED, fd_shm, 0)) == MAP_FAILED) {
+      perror ("mmap");
+    } 
+    shared_frames_->size = 0;
+    shared_frames_->offset = 0;
+    shared_frames_->indexes[0].finished = -1;
+  }
+
   void OnFrame(const webrtc::VideoFrame& frame) {
     auto buffer = frame.video_frame_buffer();
     rtc::scoped_refptr<webrtc::I420BufferInterface> buf(buffer->ToI420());
     SetSize(buf->width(), buf->height());
+
+    int frame_size = buf->width() * buf->height() * 4;;
+    if (frame_size > (CONTENT_SIZE) / 4) {
+      RTC_LOG(LERROR) << "frame_size: " << frame_size << " is too large for content_size: " << CONTENT_SIZE;   
+    }
+
+    int index = shared_frames_->size++;
+    shared_frames_->indexes[shared_frames_->size].finished = -1;
+    uint32_t offset = shared_frames_->offset;
+    if (offset + frame_size > CONTENT_SIZE) {
+        offset = 0;
+    }
+    shared_frames_->offset += frame_size;
+    shared_frames_->indexes[index].offset = offset; 
+    shared_frames_->indexes[index].length = frame_size; 
+    shared_frames_->indexes[index].timestamp = frame.timestamp();
+    libyuv::I420ToARGB(buf->DataY(), buf->StrideY(), buf->DataU(),
+            buf->StrideU(), buf->DataV(), buf->StrideV(),
+            shared_frames_->content + offset, width_ * 4, buf->width(), buf->height());
+    shared_frames_->indexes[index].finished = 1;
     
     RTC_LOG(INFO) << "[" << name_ << "] Received frame of size: " << 
         buf->width() << "x" << buf->height() << " at " << 
@@ -71,6 +151,8 @@ class VideoRenderer : public rtc::VideoSinkInterface<webrtc::VideoFrame> {
   std::string name_;
   int width_;
   int height_;
+  struct shared_frames* shared_frames_;
+  unsigned char* shared_memory_;
   rtc::scoped_refptr<webrtc::VideoTrackInterface> rendered_track_;
 };
 
