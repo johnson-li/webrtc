fc21fc828fae33768ee8cb0a9baa310eead55924
Xuebing Li <johnsonli1993@gmail.com>
2020-07-20 02:42:53 +0300

    plot point cloud

--- analysis/illustrator.py
+++ analysis/illustrator.py
@@ -2,11 +2,13 @@ import argparse
 import os
 from experiment.config import *
 from experiment.base import *
+import numpy as np
 import json
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from analysis.dataset import get_datasets, CLASSES, WAYMO_CLASSES
 from waymo_open_dataset import dataset_pb2 as open_dataset
+from waymo_open_dataset.utils import frame_utils
 from analysis.parser import parse_results_accuracy
 from experiment.logging import logging
 
@@ -71,6 +73,91 @@ def draw_frame_boxes_yolo(detections, frame_sequence, cls_list=None):
         ax.add_patch(rect)
 
 
+def draw_labels(frame, frame_sequence, ground_truth, detections):
+    text_var = plt.text(0, 0, '#%d' % frame_sequence)
+    if ground_truth:
+        labels = frame.camera_labels[0].labels
+        draw_frame_boxes_waymo(labels)
+    if detections:
+        draw_frame_boxes_yolo(detections, frame_sequence)
+    return text_var
+
+
+def rgba(r):
+    """Generates a color based on range.
+
+    Args:
+        r: the range value of a given point.
+    Returns:
+        The color for a given range
+    """
+    c = plt.get_cmap('jet')((r % 20.0) / 20.0)
+    c = list(c)
+    c[-1] = 0.5  # alpha
+    return c
+
+
+def plot_points_on_image(projected_points, rgba_func,
+                         point_size=5.0):
+    """Plots points on a camera image.
+
+    Args:
+        projected_points: [N, 3] numpy array. The inner dims are
+            [camera_x, camera_y, range].
+        rgba_func: a function that generates a color from a range value.
+        point_size: the point size.
+
+    """
+    xs = []
+    ys = []
+    colors = []
+
+    for point in projected_points:
+        xs.append(point[0])  # width, col
+        ys.append(point[1])  # height, row
+        colors.append(rgba_func(point[2]))
+
+    return plt.scatter(xs, ys, c=colors, s=point_size, edgecolors="none")
+
+
+def draw_projection(frame, range_images, camera_projections, range_image_top_pose):
+    points, cp_points = frame_utils.convert_range_image_to_point_cloud(
+        frame,
+        range_images,
+        camera_projections,
+        range_image_top_pose)
+    points_ri2, cp_points_ri2 = frame_utils.convert_range_image_to_point_cloud(
+        frame,
+        range_images,
+        camera_projections,
+        range_image_top_pose,
+        ri_index=1)
+    # 3d points in vehicle frame.
+    points_all = np.concatenate(points, axis=0)
+    points_all_ri2 = np.concatenate(points_ri2, axis=0)
+    # camera projection corresponding to each point.
+    cp_points_all = np.concatenate(cp_points, axis=0)
+    cp_points_all_ri2 = np.concatenate(cp_points_ri2, axis=0)
+
+    cp_points_all_concat = np.concatenate([cp_points_all, points_all], axis=-1)
+    cp_points_all_concat_tensor = tf.constant(cp_points_all_concat)
+
+    # The distance between lidar points and vehicle frame origin.
+    points_all_tensor = tf.norm(points_all, axis=-1, keepdims=True)
+    cp_points_all_tensor = tf.constant(cp_points_all, dtype=tf.int32)
+
+    mask = tf.equal(cp_points_all_tensor[..., 0], 1)
+
+    cp_points_all_tensor = tf.cast(tf.gather_nd(
+        cp_points_all_tensor, tf.where(mask)), dtype=tf.float32)
+    points_all_tensor = tf.gather_nd(points_all_tensor, tf.where(mask))
+
+    projected_points_all_from_raw_data = tf.concat(
+        [cp_points_all_tensor[..., 1:3], points_all_tensor], axis=-1).numpy()
+
+    return plot_points_on_image(projected_points_all_from_raw_data, rgba, point_size=2.0)
+
+
 def illustrate(ground_truth=True, prediction_path=None):
     frame_sequence = 0
     datasets = get_datasets(limit=20)
@@ -79,21 +166,23 @@ def illustrate(ground_truth=True, prediction_path=None):
         for record in dataset:
             frame = open_dataset.Frame()
             frame.ParseFromString(bytearray(record.numpy()))
-            image = frame.images[0].image
-            image = tf.image.decode_jpeg(image).numpy()
+            (range_images, camera_projections, range_image_top_pose) = \
+                frame_utils.parse_range_image_and_camera_projection(frame)
+            image = tf.image.decode_jpeg(frame.images[0].image).numpy()
+
             for p in reversed(ax.patches):
                 p.remove()
+
+            # Draw contents
             draw_frame_image(image)
-            text_var = plt.text(0, 0, '#%d' % frame_sequence)
-            if ground_truth:
-                labels = frame.camera_labels[0].labels
-                draw_frame_boxes_waymo(labels)
-            if detections:
-                draw_frame_boxes_yolo(detections, frame_sequence)
+            text_var = draw_labels(frame, frame_sequence, ground_truth, detections)
+            scatter = draw_projection(frame, range_images, camera_projections, range_image_top_pose)
+
             plt.draw()
             plt.pause(.001)
             frame_sequence += 1
             text_var.set_visible(False)
+            scatter.remove()
 
 
 def parse_args():
