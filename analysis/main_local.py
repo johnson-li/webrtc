f8632e52a7dd6c879fdfd75cac8349e16893558d
Johnson <johnsonli1993@gmail.com>
2020-08-23 00:53:38 +0300

    add analysis/main_local

---
+++ analysis/main_local.py
@@ -0,0 +1,24 @@
+import argparse
+from experiment.logging import logging_wrapper, logging
+from analysis.main import parse_results_latency, print_results_latency
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='A tool to analyse the experiment result in the localhost.')
+    parser.add_argument('-p', '--plot', action='store_true', help='Plot statics')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    path = '/tmp/webrtc/logs'
+    frames = parse_results_latency(path, 0)
+    print_results_latency(frames, path, args.plot)
+
+
+if __name__ == '__main__':
+    main()
