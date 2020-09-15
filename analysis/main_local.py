088f68c733d14f1bd4e2c1cd5d6840502af5cdcd
Johnson <johnsonli1993@gmail.com>
2020-09-15 09:02:10 +0300

    update main_local

--- analysis/main_local.py
+++ analysis/main_local.py
@@ -9,13 +9,14 @@ LOGGER = logging.getLogger(__name__)
 def parse_args():
     parser = argparse.ArgumentParser(description='A tool to analyse the experiment result in the localhost.')
     parser.add_argument('-p', '--plot', action='store_true', help='Plot statics')
+    parser.add_argument('-d', '--path', default='/tmp/webrtc/logs', help='Plot statics')
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
-    path = '/tmp/webrtc/logs'
+    path = args.path
     frames = parse_results_latency(path, 0)
     print_results_latency(frames, path, args.plot)
 
