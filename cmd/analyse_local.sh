62e359209707a5effe1d67371310a151e8d59f4f
Xuebing Li <johnsonli1993@gmail.com>
2020-10-27 21:18:58 +0200

    use conda in shell scripts

--- cmd/analyse_local.sh
+++ cmd/analyse_local.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/zsh
 
+source ~/.zshrc
+conda activate dev7
 exp_name=webrtc_exp5
 ls /mnt/wd/$exp_name | xargs -P8 -I FILE bash -c 'python -m analysis.main_local -d /mnt/wd/'$exp_name'/FILE -w yolov5s'
 
