e1da833baca480bd7c586c65fbda4618d56de0b4
Johnson <johnsonli1993@gmail.com>
2020-11-06 23:25:48 +0200

    draw outputs

--- analysis/blackhole.py
+++ analysis/blackhole.py
@@ -287,9 +287,9 @@ def illustrate(locations, bitrates, sync):
                 plt.title(f'Pour, bitrate: {bitrate} Mbps')
                 plt.show()
 
-    # draw_base_stations()
+    draw_base_stations()
     # draw_signal_strength()
-    draw_sink()
+    # draw_sink()
 
 
 
