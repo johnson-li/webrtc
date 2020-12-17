a0db816ca591585341bffbbda9a4aca6f3a9b30b
Xuebing Li <lix16@5g-mobix.research.comnet.aalto.fi>
2020-12-17 07:31:19 +0200

    update

--- cmd/analyse_local.sh
+++ cmd/analyse_local.sh
@@ -5,5 +5,6 @@ conda activate dev
 exp_name=webrtc_exp3
 concurrency=12
 weight=yolov5x
-ls ~/Data/${exp_name} | xargs -P$concurrency -I FILE bash -c 'python -m analysis.main_local -d ~/Data/'${exp_name}'/FILE -w '$weight
+# ls ~/Data/${exp_name} | xargs -P$concurrency -I FILE bash -c 'python -m analysis.main_local -d ~/Data/'${exp_name}'/FILE -w '$weight
+ls -d ~/Data/$exp_name/baseline_* | xargs -P$concurrency -I FILE bash -c 'python -m analysis.main_local -ad FILE -w '$weight
 
