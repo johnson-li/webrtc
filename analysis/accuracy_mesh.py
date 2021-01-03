e140a60c16c00119626c057dc40cc3c7f2709da4
Johnson <johnsonli1993@gmail.com>
2021-01-03 13:47:12 +0200

    update accuracy_mesh

--- analysis/accuracy_mesh.py
+++ analysis/accuracy_mesh.py
@@ -1,12 +1,10 @@
 import argparse
-import math
 import json
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from utils.cache import cache, read_cache
-from pprint import pprint
 from utils.files import get_meta, get_meta_dict
 from utils.const import get_resolution
 from utils.base import RESULT_IMAGE_PATH
@@ -50,7 +48,7 @@ def get_bbox(d):
     return d['x1'], d['y1'], d['x2'], d['y2']
 
 
-def handle_frame(detections, ground_truth):
+def handle_frame(detections, ground_truth, resolution):
     res = {'data': [],
            'detection_num': len(detections),
            }
@@ -61,8 +59,9 @@ def handle_frame(detections, ground_truth):
             cls_pred = detection['cls_pred']
             cls_pred = coco_class_to_waymo(cls_pred)
             if cls == cls_pred:
-                iou = bbox_iou(get_bbox(detection), np.array([get_bbox(gt), ]))[0]
-                ious.append((iou, detection))
+                det = np.array(get_bbox(detection)) * 1920 / resolution[0]
+                iou = bbox_iou(det, np.array([get_bbox(gt), ]))[0]
+                ious.append((iou, detection,))
         ious = filter(lambda x: x[0] > 0, ious)
         ious = sorted(ious, key=lambda x: x[0])
         if ious:
@@ -72,7 +71,10 @@ def handle_frame(detections, ground_truth):
             iou, det = 0, None
         res['data'].append({'ground_truth': gt, 'IOU': iou, 'detection': det})
     res['ground_truth_num'] = len(ground_truth)
-    res['iou_median'] = np.median([d['IOU'] for d in res['data'] if d['IOU'] > 0])
+    res['iou_list'] = [d['IOU'] for d in res['data'] if d['IOU'] > 0]
+    res['conf_list'] = [d['detection']['conf'] for d in res['data'] if d['IOU'] > 0]
+    res['iou_median'] = np.median(res['iou_list'])
+    res['conf_median'] = np.median(res['conf_list'])
     res['recognise_ratio'] = len([d for d in res['data'] if d['IOU'] > 0]) / len(ground_truth)
     res['recognise_ratio2'] = f"{len([d for d in res['data'] if d['IOU'] > 0])} / {len(ground_truth)}"
     res.pop('data')
@@ -102,14 +104,12 @@ def handle(path, weight, ground_truth, sequences=[], resolutions=[]):
         bitrate = 12000
     if f'{resolution[0]}p' not in resolutions:
         return None
-    print(f'bitrate: {bitrate}')
     dump_dir = os.path.join(path, 'dump')
     detections = load_detections(dump_dir, weight, sequences)
     detections = {int(k): [convert_detection(vv, resolution) for vv in v] for k, v in detections.items()}
     res = {}
     for seq in detections.keys():
-        res[seq] = handle_frame(detections[seq], ground_truth[seq])
-        print(f'statics: {res[seq]}')
+        res[seq] = handle_frame(detections[seq], ground_truth[seq], resolution)
     res.update({
         'is_baseline': is_baseline,
         'resolution': resolution,
@@ -141,11 +141,10 @@ def work(path, weight, sequences, resolutions):
 
 
 def main():
-    sequences = [183]
+    sequences = []
     resolutions = ['1920p']
     args = parse_args()
     res = work(args.path, args.weight, sequences, resolutions)
-    # pprint(res)
     print("Start illustration")
     illustrate(sequences, resolutions, args.path, args.weight)
 
@@ -160,9 +159,13 @@ def merge(statics, sequences):
     if not statics:
         return {}
     keys = statics[0].keys()
-    print(statics)
     for k in keys:
-        res[k] = np.nanmedian([s[k] for s in statics])
+        if k.endswith('list'):
+            res.setdefault(k, [])
+            for s in statics:
+                res[k] += s[k]
+        else:
+            res[k] = np.nanmedian([s[k] for s in statics])
     return res
 
 
@@ -171,9 +174,19 @@ def draw_diagram(key, data, resolution):
     data = {int(k): v for k, v in data.items()}
     keys = sorted(data.keys())
     values = [data[k] for k in keys]
-    plt.plot(keys, values)
-    plt.title(key)
-    plt.show()
+    if key.endswith('list'):
+        median_val = np.array([np.median(v) for v in values])
+        top_val = np.array([np.percentile(v, 90) for v in values])
+        bottom_val = np.array([np.percentile(v, 10) for v in values])
+        yerr = (median_val - bottom_val, top_val - median_val)
+        plt.bar([str(k / 1000) for k in keys], [np.median(v) for v in values],
+                yerr=yerr)
+        plt.title(key)
+        plt.show()
+    else:
+        plt.bar([str(k / 1000) for k in keys], values)
+        plt.title(key)
+        plt.show()
 
 
 def draw_figure(sequence, resolution, path, weight):
@@ -224,7 +237,7 @@ def illustrate(sequences, resolutions, path, weight):
             if seq in ('resolution', 'bitrate', 'is_baseline'):
                 continue
             data.setdefault(resolution, {}).setdefault(bitrate, {})[seq] = \
-                {'iou_median': statics['iou_median'], 'recognise_ratio': statics['recognise_ratio']}
+                {k: statics[k] for k in ['iou_median', 'recognise_ratio', 'conf_median', 'iou_list', 'conf_list']}
     data2 = {}
     for resolution, v in data.items():
         for bitrate, statics in v.items():
