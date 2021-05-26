b102f5016e08c184d53fc066784cecda5f4cb70a
johnson <johnsonli1993@gmail.com>
2021-05-26 11:06:08 +0300

    native benchmark client and server

---
+++ python_src/benchmark/client_native.py
@@ -0,0 +1,121 @@
+from benchmark.config import *
+from pathlib import Path
+import os
+from uuid import uuid4
+import argparse
+import time
+import json
+import socket
+from utils2.logging import logging
+
+logger = logging.getLogger(__name__)
+
+
+def start_probing_client(target_ip, port, duration, delay, client_id, log_path, pkg_size):
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.setblocking(0)
+        size = max(pkg_size, ID_LENGTH + PACKET_SEQUENCE_BYTES)
+        buf = bytearray(size)
+        buf[:ID_LENGTH] = client_id.encode()
+        statics = {'probing_received': [], 'probing_sent': []}
+        seq = 0
+        s.connect((target_ip, port))
+        start_ts = time.monotonic()
+        termination_num = 3
+        termination_sent = 0
+        while True:
+            try:
+                data, addr = s.recvfrom(1500)
+                sequence = int.from_bytes(data[ID_LENGTH: ID_LENGTH + PACKET_SEQUENCE_BYTES], BYTE_ORDER)
+                statics['probing_received'].append((time.monotonic(), sequence, len(data)))
+            except BlockingIOError as e:
+                pass
+            now = time.monotonic()
+            if now - start_ts > duration + termination_sent:
+                s.send('T'.encode())
+                termination_sent += 1
+            elif now - (seq * delay / 1000 + start_ts) >= -.001 and now - start_ts <= duration:
+                statics['probing_sent'].append([now, seq, len(buf)])
+                buf[ID_LENGTH: ID_LENGTH + PACKET_SEQUENCE_BYTES] = seq.to_bytes(PACKET_SEQUENCE_BYTES, BYTE_ORDER)
+                s.send(buf)
+                seq += 1
+            if termination_num == termination_sent:
+                path = os.path.join(log_path, f'probing_client_{client_id}.log')
+                with open(path, 'w+') as f:
+                    json.dump(statics, f)
+                    logger.info(f'Dump statics data to {path}')
+                    break
+
+
+def start_control_client(target_ip, port, service, client_id, delay, pkg_size):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((target_ip, port))
+        if service == 'probing':
+            s.send(json.dumps({'id': client_id,
+                               'request': {'type': 'probing', 'delay': delay, 'pkg_size': pkg_size}}).encode())
+        else:
+            logger.info(f'Unsupported service: {service}')
+            return
+        data = s.recv(1500)
+        data = json.loads(data.decode())
+        logger.info(f'Received from server: {data}')
+        return data
+
+
+def start_statics_client(target_ip, port, client_id, log_path):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((target_ip, port))
+        f = os.path.join(log_path, f'server_{client_id}.log')
+        with open(f, 'w+') as ff:
+            while True:
+                s.send(json.dumps({'id': client_id, 'request': {'type': 'statics'}}).encode())
+                data = s.recv(1500)
+                if data:
+                    data = json.loads(data.decode())
+                    logger.info(f'Received from server: {data}')
+                    if data['status'] == 1:
+                        cmd = f'scp localhost:{data["path"]} {log_path}'
+                        logger.info(f'cmd: {cmd}')
+                        os.system(cmd)
+                        break
+                time.sleep(.3)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='A UDP client to flood the server')
+    parser.add_argument('-s', '--server', default='127.0.0.1', help='The IP address of the benchmark server')
+    parser.add_argument('-p', '--port', default=DEFAULT_TCP_CONTROL_PORT, type=int,
+                        help='The port of the benchmark server')
+    parser.add_argument('-d', '--data-rate', default=DEFAULT_DATA_RATE, type=int,
+                        help='The client\'s data rate of sending packets')
+    parser.add_argument('-a', '--packet-size', default=0, type=int,
+                        help='The payload size of the UDP packets')
+    parser.add_argument('-t', '--duration', default=15, type=int, help='The duration of running the data protocol')
+    parser.add_argument('-r', '--probing-delay', default=10, type=float,
+                        help='The interval of sending continuous probing packets')
+    parser.add_argument('-l', '--logger', default='/tmp/webrtc/logs2', help='The path of statics log')
+    parser.add_argument('-b', '--service', choices=['udp_sink', 'udp_pour', 'probing'], default='udp_sink',
+                        help='Specify the type of service')
+    parser.add_argument('-f', '--fps', default=0, type=int, help='FPS')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    Path(args.logger).mkdir(parents=True, exist_ok=True)
+    client_id = str(uuid4())
+    data = start_control_client(args.server, args.port, args.service, client_id, args.probing_delay, args.packet_size)
+    if data['status'] == 1 and data['id'] == client_id:
+        logger.info(f'Found target service, type: {data["type"]}, '
+                    f'protocol: {data.get("protocol", None)}, port: {data.get("port", None)}')
+        if data['type'] == 'probing':
+            start_probing_client(args.server, data['port'], args.duration, args.probing_delay, client_id, args.logger,
+                                 args.packet_size)
+        start_statics_client(args.server, args.port, client_id, args.logger)
+    else:
+        logger.error(f"Server error: {data['message']}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ python_src/benchmark/server_native.py
@@ -0,0 +1,119 @@
+import os
+import time
+import json
+import socket
+from benchmark.config import DEFAULT_TCP_CONTROL_PORT, DEFAULT_UDP_PROBING_PORT, ID_LENGTH, BYTE_ORDER, \
+    PACKET_SEQUENCE_BYTES
+from pathlib import Path
+from multiprocessing import Process, Manager
+from utils2.logging import logging
+
+logger = logging.getLogger(__name__)
+LOG_PATH = '/tmp/webrtc/logs'
+PROBING_CLIENTS = {}
+STATICS = {}
+
+
+def start_control_server(shared):
+    logger.info(f'Running control server')
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("0.0.0.0", DEFAULT_TCP_CONTROL_PORT))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                logger.info(f'Connected by {addr}')
+                data = conn.recv(1024)
+                if not data:
+                    break
+                data = json.loads(data.decode())
+                logger.info(f'Received from client: {data}')
+                client_id = data['id']
+                request = data['request']
+                request_type = request.get('type', None)
+                fps = request.get('fps', 0)
+                if request_type == 'probing':
+                    shared['delay'] = request['delay']
+                    shared['pkg_size'] = request['pkg_size']
+                    conn.send(json.dumps({'id': client_id, 'status': 1, 'type': 'probing',
+                                          'port': DEFAULT_UDP_PROBING_PORT, 'protocol': 'UDP'}).encode())
+                elif request_type == 'statics':
+                    f = os.path.join(LOG_PATH, f'server_{client_id}.log')
+                    ff = os.path.join(LOG_PATH, f'server_{client_id}.log.finish')
+                    if not os.path.exists(ff):
+                        conn.send(json.dumps({'id': client_id, 'status': 2, 'type': 'statics',
+                                              'message': 'not ready'}).encode())
+                    else:
+                        # data = json.load(open(f))
+                        conn.send(json.dumps({'id': client_id, 'status': 1,
+                                              'type': 'statics', 'path': f}).encode())
+
+                else:
+                    conn.send(json.dumps({'status': -1, 'message': f'Invalid request type: {request_type}'}).encode())
+
+
+def start_probing_server(shared):
+    logger.info('Running probing server')
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.setblocking(0)
+        s.bind(('0.0.0.0', DEFAULT_UDP_PROBING_PORT))
+        statics = {}
+        client_id = None
+        addr = None
+        buf = bytearray(ID_LENGTH + PACKET_SEQUENCE_BYTES)
+
+        while True:
+            try:
+                data, addr_ = s.recvfrom(1500)
+                if data:
+                    if len(data) == 1 and data[0] == 'T'.encode()[0]:
+                        f = os.path.join(LOG_PATH, f'server_{client_id}.log')
+                        ff = os.path.join(LOG_PATH, f'server_{client_id}.log.finish')
+                        if not os.path.exists(f):
+                            json.dump(statics, open(f, 'w+'))
+                        with open(ff, 'w+') as ff:
+                            ff.write('1')
+                        addr = None
+                    else:
+                        if not addr:
+                            addr = addr_
+                            client_id = data[:ID_LENGTH].decode()
+                            buf[:ID_LENGTH] = client_id.encode()
+                            probing_delay = shared['delay']
+                            pkg_size = shared['pkg_size']
+                            size = max(pkg_size, ID_LENGTH + PACKET_SEQUENCE_BYTES)
+                            buf = bytearray(size)
+                            statics = {'probing_sent': [], 'probing_received': [], 'sequence': 0,
+                                       'start_ts': time.monotonic(),
+                                       'client_id': client_id, 'delay': probing_delay}
+                        if addr == addr_:
+                            client_id = data[:ID_LENGTH].decode()
+                            sequence = int.from_bytes(data[ID_LENGTH: ID_LENGTH + PACKET_SEQUENCE_BYTES], BYTE_ORDER)
+                            statics['probing_received'].append([time.monotonic() * 1000, sequence, len(data)])
+            except BlockingIOError as e:
+                pass
+            if addr:
+                now = time.monotonic()
+                waits = statics['sequence'] * statics['delay'] / 1000.0 - (now - statics['start_ts'])
+                if waits < 0.001:
+                    statics['probing_sent'].append([now * 1000, statics['sequence'], len(buf)])
+                    buf[ID_LENGTH: ID_LENGTH + PACKET_SEQUENCE_BYTES] = \
+                        statics['sequence'].to_bytes(PACKET_SEQUENCE_BYTES, BYTE_ORDER)
+                    s.sendto(buf, addr)
+                    statics['sequence'] += 1
+
+
+def main():
+    Path(LOG_PATH).mkdir(parents=True, exist_ok=True)
+    with Manager() as manager:
+        shared = manager.dict()
+        control_process = Process(target=start_control_server, args=(shared,))
+        probing_process = Process(target=start_probing_server, args=(shared,))
+        control_process.start()
+        probing_process.start()
+        control_process.join()
+        probing_process.join()
+
+
+if __name__ == '__main__':
+    main()
