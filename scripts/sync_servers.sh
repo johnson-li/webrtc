a82638ffdce66fc1fcf88b24d1291e8c6d9e99d1
Johnson <johnsonli1993@gmail.com>
2021-12-08 01:20:43 +0200

    update

--- scripts/sync_servers.sh
+++ scripts/sync_servers.sh
@@ -1,6 +1,6 @@
 for host in lab4 lab6
 do
-  for f in sync_client peerconnection_client_headless peerconnection_server_headless
+  for f in sync_client sync_server peerconnection_client_headless peerconnection_server_headless
   do
     rsync ~/Workspace/webrtc/src/out/Default/$f $host:~/bin/
   done
