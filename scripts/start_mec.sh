5e56be3f2044cd34b4d56b11faa02f86d4968273
Johnson <johnsonli1993@gmail.com>
2021-12-08 17:20:50 +0200

    update

--- scripts/start_mec.sh
+++ scripts/start_mec.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-mv /tmp/webrtc/logs /tmp/webrtc/$(date)logs
-mkdir -p /tmp/webrtc/logs/frames1
-mkdir -p /tmp/webrtc/logs/frames2
-mkdir -p /tmp/webrtc/logs/frames3
-mkdir -p /tmp/webrtc/logs/frames4
-mkdir -p /tmp/webrtc/logs/frames5
+mv /tmp/webrtc/logs "/tmp/webrtc/$(date).logs"
+mkdir -p /tmp/webrtc/logs/frames
 
 
 #session_name=dns
@@ -24,12 +20,13 @@ session_name=mobix
 tmux has-session -t ${session_name} 2> /dev/null; if [[ $? == 0 ]]; then tmux kill-session -t ${session_name}; fi
 tmux new-session -ds ${session_name}
 tmux send-key -t ${session_name}:0 'cd ~; conda activate dev' Enter
-for i in $(seq 4); do
+for i in $(seq 6); do
     tmux new-window -t ${session_name}:$i
     tmux send-key -t ${session_name}:$i 'cd ~; conda activate dev' Enter
 done
 tmux send-key -t ${session_name}:0 'while true; do ./bin/sync_server; done' Enter
 tmux send-key -t ${session_name}:1 './bin/peerconnection_server_headless --port 8881' Enter
 tmux send-key -t ${session_name}:3 'cd ~/Workspace/darknet; ./darknet detector test mapillary.data mapillary.cfg mapillary_33000.weights -dont_show -ext_output ~/Workspace/yolov5/inference/images/bus.jpg -path /tmp/webrtc/logs/frames' Enter
+tmux send-key -t ${session_name}:4 'cd ~/Workspace/eatw; python yolo_server.py' Enter
 tmux send-key -t ${session_name}:2 './bin/peerconnection_client_headless --receiving_only --name RECEIVER --dump /tmp/webrtc/logs/frames --server 127.0.0.1 --port 8881 > /tmp/webrtc/logs/client1.log 2>&1' Enter
 
