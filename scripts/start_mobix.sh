a5b37228278b551a458d155f70682348fcf1659d
johnson <johnsonli1993@gmail.com>
2022-05-18 12:00:58 +0200

    update

--- scripts/start_mobix.sh
+++ scripts/start_mobix.sh
@@ -12,8 +12,8 @@ for i in $(seq 4); do
     tmux new-window -t ${session_name}:$i
     tmux send-key -t ${session_name}:$i 'conda activate dev' Enter
 done
-tmux send-key -t ${session_name}:0 'authbind python -m dns.server > /tmp/webrtc/logs/$(date +"%Y-%m-%d-%H-%M-%S").dns.log' Enter
-tmux send-key -t ${session_name}:1 'python -m dns.monitor_server' Enter
+tmux send-key -t ${session_name}:0 'cd ~/Workspace/eatw; authbind python -m dns.server > /tmp/webrtc/logs/$(date +"%Y-%m-%d-%H-%M-%S").dns.log' Enter
+tmux send-key -t ${session_name}:1 'cd ~/Workspace/eatw; python -m dns.monitor_server' Enter
 
 
 session_name=mobix
